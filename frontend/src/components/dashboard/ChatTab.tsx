bd5ecfe8072336978b961d3cecc6608347f098e4
Dawson <dawsonpowell07@gmail.com>
2025-12-18 14:10:04 -0500

    working nextauth with aws cognitio

--- frontend/src/components/dashboard/ChatTab.tsx
+++ frontend/src/components/dashboard/ChatTab.tsx
@@ -1,9 +1,12 @@
 import { CopilotChat } from "@copilotkit/react-ui"; 
+import { CopilotKit } from "@copilotkit/react-core"; 
 
 export default function ChatTab() {
   return (
     <div className="space-y-6">
-      <CopilotChat></CopilotChat>
+      <CopilotKit runtimeUrl="/api/copilotkit" agent="strands_agent">
+        <CopilotChat/>
+      </CopilotKit>
     </div>
   );
 }
